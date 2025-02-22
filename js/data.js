09746c18e62ab108ff35c2ab6d36f2d04ab37c82
Edisson Torres <87282774+EDDIT8@users.noreply.github.com>
2025-02-22 16:49:59 -0500

    Add Car Porsche to file data.js

--- js/data.js
+++ js/data.js
@@ -87,6 +87,121 @@ const carsData = {
                 }
             }
         },
+
+        {
+            id: "koenigsegg-agera-r",
+            name: "Porsche 911.2",
+            brand: {
+                name: "Porsche",
+                logo: "assets/logos/koenigsegg.svg"
+            },
+            year: 2013,
+            category: "Street Tire 2",
+            description: "Excelente auto para estabilidad con la mejor velocidad maxima y el mejor rendimiento en curvas",
+            image: "assets/cars/agera-r.jpg",
+            tuning: {
+                transmission: {
+                    title: "Transmisión",
+                    settings: {
+                        finalRatio: {
+                            label: "Relación final",
+                            value: 0,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "0% Corto",
+                            rightLabel: " Largo",
+                        }
+                    }
+                },
+                tires: {
+                    title: "Neumáticos",
+                    settings: {
+                        frontGrip: {
+                            label: "Agarre delantero",
+                            value: -5,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "-5%",
+                            rightLabel: ""
+                        },
+                        rearGrip: {
+                            label: "Agarre trasero",
+                            value: 0,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "0%",
+                            rightLabel: ""
+                        }
+                    }
+                },
+                suspension: {
+                    title: "Suspensión",
+                    settings: {
+                        frontSuspension: {
+                            label: "Suspensión delantera",
+                            value: 1,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "Suave",
+                            rightLabel: "Rígido"
+                        },
+                        rearSuspension: {
+                            label: "Suspensión trasera",
+                            value: 5,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "Suave",
+                            rightLabel: "Rígido"
+                        },
+                        frontCompression: {
+                            label: "Compresión de amortiguador delantero",
+                            value: -10,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "Suave",
+                            rightLabel: "Rígido"
+                        },
+                        rearCompression: {
+                            label: "Compresión de amortiguador trasero",
+                            value: -15,
+                            min: -100,
+                            max: 100,
+                            leftLabel: "Suave",
+                            rightLabel: "Rígido"
+                        },
+                        frontRebound : {
+                            label: "Rebote de amortiguador delantero",
+                            value: +10,
+                            min: -100,
+                            max: +100,
+                            leftLabel: "Suave",
+                            rightLabel: "Rígido",
+                        }
+                    }
+                },
+                Barras: {
+                    title: "Barras Estabilizadoras",
+                    settings: {
+                        front: {
+                            label: "Barra estabiizadora delantera",
+                            value: "+3%",
+                            min: "-20%",
+                            max: "+10%",
+                            leftLabel: "+3% Suave",
+                            rightLabel: "Rigido"
+                        },
+                        Rear: {
+                            label: "Barra estabiizadora Trasera",
+                            value: "-3%",
+                            min: "-20%",
+                            max: "+10%",
+                            leftLabel: "-3% Suave",
+                            rightLabel: "Rigido"
+                        }
+                    }
+                }
+            }
+        },
         // Añade más autos aquí siguiendo la misma estructura
     ]
 };
