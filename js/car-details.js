2f7e1cb59f9b6bde05be22b22e100cf86046850f
Edisson Torres <87282774+EDDIT8@users.noreply.github.com>
2025-02-22 17:04:59 -0500

    Update car-details.js

--- js/car-details.js
+++ js/car-details.js
@@ -1,5 +1,3 @@
-import { carsData } from './data.js';
-
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const carId = urlParams.get('id');
