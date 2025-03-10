8b972c64559b59dbc35778f1e4377700976139db
Edisson Torres <87282774+EDDIT8@users.noreply.github.com>
2025-03-10 22:04:05 +0000

    fix bug in line 575

--- js/main.js
+++ js/main.js
@@ -572,7 +572,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/TCM/service-worker.js').then(registration => {
       console.log('ServiceWorker registrado con éxito:', registration);
     }).catch(error => {
       console.log('Error en el registro del ServiceWorker:', error);
